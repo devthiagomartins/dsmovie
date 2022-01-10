34c234b60f27e21094b645bde35419890789812d
Thiago Martins <devthiagomartins@gmail.com>
2022-01-10 11:03:52 -0300

    Navigation buttons

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,4 +1,5 @@
 import MovieScore from "components/MovieScore";
+import { Link } from "react-router-dom";
 
 function MovieCard() {
 
@@ -8,7 +9,7 @@ function MovieCard() {
     title: "The Witcher",
     count: 2,
     score: 4.5
-};
+  };
 
   return (
 
@@ -17,7 +18,9 @@ function MovieCard() {
       <div className="dsmovie-card-bottom-container">
         <h3>{movie.title}</h3>
         <MovieScore />
-        <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+        <Link to={`/form/${movie.id}`}>
+          <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+        </Link>
       </div>
     </div>
   );
